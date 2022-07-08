abf68e70d67dc68c09181ed57b36d072b5cb6245
mralibatu <alibatuhan404@gmail.com>
2022-07-08 11:58:15 +0300

    first commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Fibonacci {
+    static int fib(int n) {
+        if (n==1 || n==2){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayi giriniz : ");
+        int sayi = input.nextInt();
+        System.out.println(fib(sayi));
+    }
+}
